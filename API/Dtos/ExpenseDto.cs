342e8f41f0ccd935b0151ab2df7b7859ade3ee91
Rafael Moraes <rafaelmrslima@gmail.com>
2020-01-01 19:52:11 -0300

    organizing folders - API

--- API/Dtos/ExpenseDto.cs
+++
@@ -1,8 +0,0 @@
-namespace API.Dtos
-{
-    public class ExpenseDto
-    {
-        public string Description { get; set; }
-        public decimal Value { get; set; }
-    }
-}
--- API/Dtos/IncomeDto.cs
+++
@@ -1,8 +0,0 @@
-namespace API.Dtos
-{
-    public class IncomeDto
-    {
-        public string Description { get; set; } 
-        public decimal Value { get; set; }
-    }
-}
