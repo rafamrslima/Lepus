31731c0b18a9847a77892e0b97ca6037149b8e28
Rafael Moraes <rafaelmrslima@gmail.com>
2020-01-04 23:01:54 -0300

    Making periodinfo be dinamic

--- APP/src/app/components/periodInfo/periodInfo.component.ts
+++ APP/src/app/components/periodInfo/periodInfo.component.ts
@@ -9,12 +9,44 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
 export class PeriodInfoComponent implements OnInit {
 
   @Output() periodChanged = new EventEmitter<string>();
-
+  years: number[] = [];
+  months: object[] = [];
+ 
   constructor(private localStorageService: LocalStorageService) { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    this.setDatesDropdown(); 
     this.onChangeYear(new Date().getFullYear().toString());
     this.onChangeMonth((new Date().getMonth() + 1 ).toString());
+  }
+
+  setDatesDropdown(){
+    var year = new Date().getFullYear();
+    this.years.push(year);
+
+    let currentMonth = new Date().getMonth();
+    var month = {
+      'number': currentMonth,
+      'name' : this.getMonthName(currentMonth)
+    }
+    this.months.push(month);
+ 
+    for (let i = 1; i < 6; i++){
+      year = year + 1;
+      this.years.push(year);
+
+      currentMonth = currentMonth + 1;
+      month = {
+        'number': currentMonth,
+        'name' : this.getMonthName(currentMonth)  
+      } 
+      this.months.push(month);
+    }
+  }
+
+  getMonthName(month: number) {
+    const date = new Date(2000, month, 1); 
+    return date.toLocaleString('en-US', { month: 'long' }); 
   }
 
   onChangeYear(year: string) {
@@ -25,5 +57,5 @@ export class PeriodInfoComponent implements OnInit {
   onChangeMonth(month: string) {
     this.localStorageService.setMonth(month);
     this.periodChanged.emit();
-  }
+  } 
 }
